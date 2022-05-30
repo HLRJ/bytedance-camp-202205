cab3b7f5df25c860d8bff30bc4d433fa6949e13c
HLRJ <befun@vip.qq.com>
2022-05-30 22:38:31 +0800

    chore: add code comments

--- 06/course/easy_note/cmd/note/main.go
+++ 06/course/easy_note/cmd/note/main.go
@@ -34,9 +34,9 @@ import (
 )
 
 func Init() {
-	tracer2.InitJaeger(constants.NoteServiceName)
-	rpc.InitRPC()
-	dal.Init()
+	tracer2.InitJaeger(constants.NoteServiceName) //jaeger初始化
+	rpc.InitRPC()                                 //需要调取user的一些信息
+	dal.Init()                                    //dal层初始化
 }
 
 func main() {
@@ -48,17 +48,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	Init()
+	Init() //显式初始化
 	svr := note.NewServer(new(NoteServiceImpl),
 		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: constants.NoteServiceName}), // server name
 		server.WithMiddleware(middleware.CommonMiddleware),                                             // middleWare
 		server.WithMiddleware(middleware.ServerMiddleware),
 		server.WithServiceAddr(addr),                                       // address
 		server.WithLimit(&limit.Option{MaxConnections: 1000, MaxQPS: 100}), // limit
-		server.WithMuxTransport(),                                          // Multiplex
-		server.WithSuite(trace.NewDefaultServerSuite()),                    // tracer
-		server.WithBoundHandler(bound.NewCpuLimitHandler()),                // BoundHandler
-		server.WithRegistry(r),                                             // registry
+		server.WithMuxTransport(),                                          // Multiplex  开启连接多路复用
+		server.WithSuite(trace.NewDefaultServerSuite()),                    // tracer  opentracing
+		server.WithBoundHandler(bound.NewCpuLimitHandler()),                // BoundHandler  cpu限流器
+		server.WithRegistry(r),                                             // registry 服务注册的配置进去
 	)
 	err = svr.Run()
 	if err != nil {
